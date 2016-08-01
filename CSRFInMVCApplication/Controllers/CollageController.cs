bd0d5cf09182e81ff1f8a83559efaa80ef7ff78e
MuneshSharma <muneshs16@gmail.com>
2016-08-01 17:03:43 +0530

    CSRF in mvc application

---
+++ CSRFInMVCApplication/Controllers/CollageController.cs
@@ -0,0 +1,33 @@
+﻿using CSRFInMVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSRFInMVCApplication.Controllers
+{
+    public class CollageController : Controller
+    {
+        //
+        // GET: /Collage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult collageInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult collageInfo(CollageInfo _clgInfo)
+        {
+            return View(_clgInfo);
+        }
+    }
+}
---
+++ CSRFInMVCApplication/Models/CollageInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CSRFInMVCApplication.Models
+{
+    public class CollageInfo
+    {
+        [Key]
+        public int CollageID { get; set; }
+        [Required(ErrorMessage = " please Enter Name")]
+        public string CollageName { get; set; }
+        [Required(ErrorMessage = "pleaes Enter Address")]
+        public string CollageAddress { get; set; }
+        [Required(ErrorMessage = "please Enter Department")]
+        public string CollageDepartment { get; set; }
+
+    }
+}
